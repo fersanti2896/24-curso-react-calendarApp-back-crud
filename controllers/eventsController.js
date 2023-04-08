c2fb50a4aaad86c036aeb77011452daf2b542c09
Fernando Nicolás <fersanti2896@gmail.com>
2023-04-08 02:39:22 -0600

    Actualizando un evento en eventsController.js

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -32,11 +32,46 @@ const createEvent = async( req, res = response ) => {
     }
 }
 
-const updateEvent = ( req, res = response ) => {
-    res.status(201).json({
-        ok: true,
-        msg: 'updateEvent'
-    });
+const updateEvent = async( req, res = response ) => {
+    const eventId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        const event = await EventoModel.findById(eventId);
+
+        if( !event ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'El evento no existe por ese id.'
+            }); 
+        }
+
+        if( event.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tienes privilegio de editar este evento.'
+            });
+        }
+
+        const newEvent = {
+            ...req.body, 
+            user: uid
+        }
+
+        const eventUpdate = await EventoModel.findByIdAndUpdate( eventId, newEvent, { new: true } );
+
+        res.status(201).json({
+            ok: true,
+            event: eventUpdate
+        });        
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador.'
+        })
+    }
 }
 
 const deleteEvent = ( req, res = response ) => {
